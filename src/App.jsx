a662bca098ed91bb4f079497c4580d2c580a4814
那須凜太郎 <tl221278@cis.fukuoka-u.ac.jp>
2023-09-29 01:13:44 +0900

    コミットメッセージ

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState,useCallback } from 'react';
 import './App.css';
 import {ChildArea} from './components/ChildArea';
 import { StyledComponents } from './components/StyledComponents';
+import axios from 'axios';
 
 
 export default function App() {
@@ -13,6 +14,24 @@ export default function App() {
   const onChangeText = (e) =>setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
   const onClickClose = useCallback(() => setOpen(false),[]);
+  const onClickUsers = () =>{
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  const onClickUser1 = () =>{
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
   return (
     <>
       <div className="App">
@@ -21,7 +40,6 @@ export default function App() {
         <ChildArea open={open} close={onClickClose}/>
       </div>
       <div>
-        <StyledComponents />
       </div>
       <BrowserRouter>
         <div className='transition'>
@@ -30,6 +48,10 @@ export default function App() {
           <Link to="/page1">Page1</Link> 
           <br /> 
           <Link to="/page2">Page2</Link>    
+        </div>
+        <div>
+          <button onClick={onClickUsers}>users</button>
+          <button onClick={onClickUser1}>id=1のuser</button>
         </div>
         <Router />
       </BrowserRouter>
